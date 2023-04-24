8de5d17c1facfa66d5ea4c86edaa7e80e764b7bc
Uladzislau Lukashevich <lukashevichuladzislau@gmail.com>
2023-04-25 01:06:46 +0200

    First commit

---
+++ src/swiat/organizm/organizmy/zwierze/Owca.java
@@ -0,0 +1,40 @@
+package swiat.organizm.organizmy.zwierze;
+
+import hex.swiat.SwiatHex;
+import panel.MainPanel;
+import swiat.organizm.organizmy.Zwierze;
+
+import java.util.Random;
+
+public class Owca extends Zwierze {
+
+    public Owca(MainPanel swiat){
+        sila = 4;
+        inicjatywa = 4;
+        lifeTime = 0;
+        alive = true;
+        this.swiat = swiat;
+        polozenie = randomizePolozenie();
+        rysowanie();
+    }
+
+    public Owca(MainPanel swiat, int x, int y){
+        sila = 4;
+        inicjatywa = 4;
+        lifeTime = 0;
+        alive = true;
+        this.swiat = swiat;
+        polozenie[0] = x; polozenie[1] = y;
+        rysowanie();
+    }
+
+    @Override
+    public void rysowanie() {
+        swiat.setOnBoard(getPolozenie()[0], getPolozenie()[1], 2);
+    }
+
+    @Override
+    public String getTypeOfOrganizm() {
+        return "Owca";
+    }
+}
